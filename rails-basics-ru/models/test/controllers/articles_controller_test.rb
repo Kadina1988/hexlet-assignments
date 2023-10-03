0e502dc50945159f1c3cc64579809f3d91969c25
@hexlet/cli <support@hexlet.io>
2023-10-03 12:36:30 +0300

    submit rails-basics-ru/models

--- rails-basics-ru/models/test/controllers/articles_controller_test.rb
+++ rails-basics-ru/models/test/controllers/articles_controller_test.rb
@@ -1,7 +1,21 @@
-require "test_helper"
+# frozen_string_literal: true
 
-class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+require 'test_helper'
+
+class ArticlesTest < ActionDispatch::IntegrationTest
+  setup do
+    @article = articles(:one)
+  end
+
+  test 'should get index' do
+    get articles_url
+
+    assert_response :success
+  end
+
+  test 'should show article' do
+    get article_url @article
+
+    assert_response :success
+  end
 end
