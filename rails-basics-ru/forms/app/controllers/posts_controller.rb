c3dc02eee41bab3e4de74867ce0e7f016006f7fd
@hexlet/cli <support@hexlet.io>
2023-10-11 21:14:46 +0300

    submit rails-basics-ru/forms

---
+++ rails-basics-ru/forms/app/controllers/posts_controller.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :summary, :published)
+  end
+end
