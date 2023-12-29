7d8bdb5d8a4a3cc957b93c908b6013e3f5580daa
@hexlet/cli <support@hexlet.io>
2023-12-29 17:34:18 +0300

    submit rails-real-ru/system-tests

---
+++ rails-real-ru/system-tests/test/system/posts_test.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+# BEGIN
+class PostsTest < ApplicationSystemTestCase
+  setup do
+    @post = posts(:one)
+  end
+
+  test 'viewing the posts' do
+    visit posts_path
+    assert_selector 'h1', text: 'Posts'
+  end
+
+  test 'viewing the post' do
+    visit post_path(@post)
+    assert_selector 'h1', text: "#{@post.title}"
+  end
+
+  test 'should create Post' do
+    visit posts_path
+
+    click_on 'New Post'
+
+    fill_in 'Title', with: 'Creating a new Post'
+    fill_in 'Body', with: 'Creating body'
+    click_on 'Create Post'
+
+    assert_text 'Post was successfully created.'
+  end
+
+  test 'should update Post' do
+    visit post_path(@post)
+
+    find('#my-link').click
+    fill_in 'Title', with: 'Update the Post'
+    fill_in 'Body', with: 'new body'
+    click_on 'Update'
+
+    assert_text 'Post was successfully updated.'
+  end
+
+  test 'should delete Post' do
+    visit post_path(@post)
+
+    page.accept_confirm do
+      click_on 'Delete', match: :first
+    end
+
+    assert_text 'Post was successfully destroyed.'
+  end
+end
+# END
