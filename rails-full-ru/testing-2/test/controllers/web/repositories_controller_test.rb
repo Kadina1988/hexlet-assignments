b901c6ee7e01cb9fd3ab1d511d69172131f81781
@hexlet/cli <support@hexlet.io>
2024-02-24 21:45:08 +0300

    submit rails-full-ru/testing-2

---
+++ rails-full-ru/testing-2/test/controllers/web/repositories_controller_test.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Web::RepositoriesControllerTest < ActionDispatch::IntegrationTest
+  # BEGIN
+  setup do
+    @repo = repositories :one
+
+    @attrs = {
+      link: 'https://github.com/railsware/js-routes'
+    }
+  end
+
+  test 'should create' do
+    response = load_fixture('files/response.json')
+    stub_request(:get, 'https://api.github.com/repos/railsware/js-routes')
+    .to_return(
+      status: 200,
+      body: response,
+      headers: { 'Content-Type' => 'application/json' }
+    )
+
+    post repositories_url, params: { repository: @attrs }
+
+    repository = Repository.find_by @attrs
+    debugger
+
+    assert { repository }
+    assert { repository.description.present? }
+    assert_redirected_to repository_url(repository)
+  end
+  # END
+end
